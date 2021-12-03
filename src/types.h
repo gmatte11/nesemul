8ed71c19bfaeb69c3315bd59b029ea503ae69f17
Guillaume Matte <gmatte11@gmail.com>
2021-12-03 11:38:41 -0500

    Replaced DebugBreak by intrinsic __debugbreak

--- src/types.h
+++ src/types.h
@@ -3,12 +3,6 @@
 
 #include <cstdint>
 
-#define WIN32_LEAN_AND_MEAN
-#define VC_EXTRALEAN
-#include <windows.h>
-#undef min
-#undef max
-
 typedef uint8_t byte_t;
 typedef uint16_t address_t;
 
@@ -45,8 +39,8 @@ struct register_t
 };
 
 
-#define BREAKPOINT DebugBreak()
+#define BREAKPOINT __debugbreak()
 #define DEOPTIMIZE __pragma(optimize("",off))
 
-#define ASSERT(X) { if(!(X)) { DebugBreak(); } }
+#define ASSERT(X) { if(!(X)) { __debugbreak(); } }
 #endif // __NESEMUL_TYPES_H__
